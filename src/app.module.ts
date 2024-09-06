4f114b506f80ac83ce797eb504642e27ea436182
Omid Nasiri <omid.nasiri.dev@gmail.com>
2024-09-07 02:27:59 +0330

    feat: leverage config module for db connection info

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,33 @@ import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { APP_PIPE } from '@nestjs/core';
 import { TodoModule } from './todo/todo.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/raychat-todo'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: (ENV == 'production') ? true : false
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService) => {
+        const host = configService.get('MONGO_HOST');
+        const port = configService.get('MONGO_PORT');
+        const user = configService.get('MONGO_USER');
+        const password = configService.get('MONGO_PASS');
+        const database = configService.get('MONGO_DATABASE');
+        if (user && password) {
+          return {
+            uri: `mongodb+srv://${user}:${password}@${host}:${port}/${database}`,
+          };
+        }
+        return { uri: `mongodb://${host}:${port}/${database}` };
+      },
+    }),
     UserModule,
     TodoModule,
   ],
